74074daa5fdba73b840086ac763b70cfa4457997
Osama Bin Tarique <133800713+osama-786@users.noreply.github.com>
2023-09-07 16:41:35 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,9 +1,12 @@
+//Requiring the mongoose module.
 const mongoose = require("mongoose");
+//Creating a Schema variable and assign it the Schema class from mongoose.
 const userSchema = new mongoose.Schema({
     username:{
         type: String
     }
 })
-
+// Creating a mongoose model with the preferred name of the collection and the UserSchema created above.
 const User = mongoose.model('UserData',userSchema);
+//Exporting Model.
 module.exports = User
