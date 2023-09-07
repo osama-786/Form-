e27ae027bca51851e36b708b874aeac6e92e20d8
Osama Bin Tarique <133800713+osama-786@users.noreply.github.com>
2023-09-07 16:40:13 +0530

    Update address.js

--- model/address.js
+++ model/address.js
@@ -1,9 +1,12 @@
+//Requiring the mongoose module.
 const mongoose = require("mongoose");
+//Creating a Schema variable and assign it the Schema class from mongoose.
 const userSchema1 = new mongoose.Schema({
     address:{
         type: String
     }
 })
-
+// Creating a mongoose model with the preffered name of collection and the UserSchema created above.
 const Address = mongoose.model('Address',userSchema1);
+//Exporing Model.
 module.exports = Address
